0f812efa373d430a447b6217f7888cf01889a073
Gabriel Litaiff <gabriellitaiff@hotmail.com>
2024-12-19 22:39:23 +0000

    Refactor

--- tests/playground.spec.js
+++ tests/playground.spec.js
@@ -3,55 +3,37 @@ const { test, expect } = require('@playwright/test');
 const { USERS } = require('../constants/users');
 
 test.describe('Login Tests', () => {
-  const performLogin = async (page, username, password) => {
+  const performLogin = async (page, user) => {
     await page.goto('https://playground-drab-six.vercel.app/');
 
     // Click the login link.
     await page.getByRole('link', { name: 'login' }).click();
 
     // Fill in login fields
-    await page.getByPlaceholder('Digite seu usuário').fill(username);
-    await page.getByPlaceholder('Digite sua senha').fill(password);
+    await page.getByPlaceholder('Digite seu usuário').fill(user.username);
+    await page.getByPlaceholder('Digite sua senha').fill(user.password);
 
     // Submit the form
     await page.getByRole('button', { name: 'Logar' }).click();
+    await expect(page.getByText(user.message)).toBeVisible();
   };
 
   test('Login with a regular user', async ({ page }) => {
-    const { username, password } = USERS.regular;
-    await performLogin(page, username, password);
-
-    // Verify successful login
-    await expect(page).toHaveURL(/dashboard/);
-    await expect(page.getByText('Usuário teste logado')).toBeVisible();
+    await performLogin(page, USERS.regular);
 
     // Click the logout link.
     await page.getByRole('button', { name: 'logout' }).click();
   });
 
   test('Login with a blocked user', async ({ page }) => {
-    const { username, password } = USERS.blocked;
-    await performLogin(page, username, password);
-
-    // Verify error message
-    await expect(page.getByText('Usuário bloqueado!')).toBeVisible();
+    await performLogin(page, USERS.blocked);
   });
 
   test('Login with incorrect password', async ({ page }) => {
-    const { username, password } = USERS.wrongPassword;
-    await performLogin(page, username, password);
-
-    // Verify error message
-    await expect(
-      page.getByText('Usuário ou senha estão incorretos!')
-    ).toBeVisible();
+    await performLogin(page, USERS.wrongPassword);
   });
 
   test('Login with invalid user', async ({ page }) => {
-    const { username, password } = USERS.invalid;
-    await performLogin(page, username, password);
-
-    // Verify error message
-    await expect(page.getByText('Usuário não encontrado!')).toBeVisible();
+    await performLogin(page, USERS.invalid);
   });
 });
