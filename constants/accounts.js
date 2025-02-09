6c1ed391a2412016dbf1d88e6445d18c1db1e4ee
Gabriel Litaiff <gabriellitaiff@hotmail.com>
2025-02-09 23:45:18 +0000

    page objects

---
+++ constants/accounts.js
@@ -0,0 +1,10 @@
+const ACCOUNTS = {
+  valid: {
+    nome: 'Bruno',
+    email: 'bruno@123email.com',
+    senha: '1234',
+    pais: 'brazil',
+    genero: 'Masculino',
+    hobbies: ['Ler', 'Jogos', 'Viajar'],
+  },
+};
