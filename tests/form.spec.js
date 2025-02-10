47fcb992b2cbe8c49e71b992d5398e72f59a0b09
Gabriel Litaiff <gabriellitaiff@hotmail.com>
2025-02-10 21:29:55 +0000

    page objects

--- tests/form.spec.js
+++ tests/form.spec.js
@@ -1,38 +1,27 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+const { ACCOUNTS } = require('../constants/accounts');
 const { FormPage } = require('../pages/formPage');
 
 test.describe('Form Tests', () => {
   test('Submit form with valid data', async ({ page }) => {
     const formPage = new FormPage(page);
     await formPage.navegarForm();
-    await formPage.preencherFormulario({
-      nome: 'João Silva',
-      email: 'joao@email.com',
-      senha: 'Isso é um teste!',
-    });
+    await formPage.preencherFormulario(ACCOUNTS.valid);
     await formPage.submeterFormulario();
   });
 
   test('Submit form with missing required fields', async ({ page }) => {
     const formPage = new FormPage(page);
     await formPage.navegarForm();
-    await formPage.preencherFormulario({
-      nome: '',
-      email: '',
-      senha: '',
-    });
+    await formPage.preencherFormulario(ACCOUNTS.completeRegistration);
     await formPage.submeterFormulario();
   });
 
   test('Submit form with invalid email', async ({ page }) => {
     const formPage = new FormPage(page);
     await formPage.navegarForm();
-    await formPage.preencherFormulario({
-      nome: 'Maria Souza',
-      email: 'email-invalido',
-      senha: 'Teste com email inválido.',
-    });
+    await formPage.preencherFormulario(ACCOUNTS.invalid);
     await formPage.submeterFormulario();
   });
 });
